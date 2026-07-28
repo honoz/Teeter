c99af8dc5886b2495a56c14367cc448a77c757f0
honoz <honoz@qq.com>
2026-07-28 12:59:11 +0800

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     packaging {
         resources {
             excludes += "DebugProbesKt.bin"
